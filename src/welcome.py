fd4534786de8603d9fc2d9e59d8c659997ea222b
Adam Le Bay <adam.lb@hotmail.com>
2015-10-29 17:27:39 +0000

    menu bar links and templates + background

--- src/welcome.py
+++ src/welcome.py
@@ -3,11 +3,19 @@ app = Flask (__name__)
 
 @app.route("/home/")
 def home():
-  return render_template('header.html')
+  return render_template('home.html')
 
 @app.route("/home/players")
 def players():
   return render_template('players.html')
+
+@app.route("/home/clubs")
+def clubs():
+  return render_template('clubs.html')
+
+@app.route("/home/tow")
+def tow():
+  return render_template ('tow.html')
 
 @app.route('/home/<name>')
 def hello(name=None):
