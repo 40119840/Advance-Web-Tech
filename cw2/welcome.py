ae220f79eb3b6a729bcbe2425bd791775a5bde44
40119840 <40119840@live.napier.ac.uk>
2015-11-23 19:15:34 +0000

    added a database query to fetch data

--- cw2/welcome.py
+++ cw2/welcome.py
@@ -15,6 +15,12 @@ def get_db():
         g.db = db
     return db
 
+def query_db(query, args=(), one=False):
+    db = get_db()
+    cur = db.execute(query, args)
+    rv = [dict((cur.description[idx][0], value) for idx, value in enumerate(row)) for row in cur.fetchall()]
+    return (rv[0] if rv else None) if one else rv    
+
 @app.teardown_appcontext
 def close_db_connection(exception):
     db = getattr(g,'db',None)
