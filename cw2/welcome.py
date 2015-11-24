47006b8f56b6a256c1249fff625dfa2f43fcfdb8
Adam Le Bay <adam.lb@hotmail.com>
2015-11-24 22:03:48 +0000

    quick commit

--- cw2/welcome.py
+++ cw2/welcome.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3i
 from sys import argv
 from flask import Flask, request, render_template, g, redirect, session
 from contextlib import closing
@@ -11,7 +11,7 @@ db_location = 'var/data.db'
 
 #database functions
 def get_db():
-    db = getattr(g, 'db',None )
+    db = getattr(g, 'db', None)
     if db is None:
         db = sqlite3.connect(db_location)
         g.db = db
@@ -30,8 +30,7 @@ def close_db_connection(exception):
         db.close()
 
 def init_db():
-    with app.app_context():
-        db = get_db()
+    with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit
