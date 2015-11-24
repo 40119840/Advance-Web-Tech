a98ed4e64178737da1db9f6f0de9f69ff90f1f78
40119840 <40119840@live.napier.ac.uk>
2015-11-24 22:35:22 +0000

    Update schema.sql

--- cw2/schema.sql
+++ cw2/schema.sql
@@ -4,7 +4,6 @@ DROP TABLE if EXISTS users;
      idUser integer PRIMARY KEY autoincrement,
      username varchar(50) NOT NULL,
      password varchar(300) NOT NULL,
-     points varchar (200) DEFAULT "1",
 ); 
 
 /*
