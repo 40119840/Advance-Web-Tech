ebea327db15d6f80fde0bb85dcfad815ddc49e7e
40119840 <40119840@live.napier.ac.uk>
2015-11-26 13:31:56 +0000

    welcome.py database working

--- cw2/welcome.py
+++ cw2/welcome.py
@@ -2,9 +2,8 @@ from flask import Flask, redirect, url_for, abort, request, render_template, ses
 import sqlite3
 import ConfigParser
 from flask.ext.wtf import Form
-from wtforms import TextField, HiddenField
+from wtforms import TextField
 from contextlib import closing
-from urlparse import urlparse, urljoin
 
 #app create
 app = Flask(__name__)
@@ -12,29 +11,13 @@ db_location = 'var/data.db'
 #database functions
 
 
-def connect_db():
-  connect = sqlite3.connect(db_location)
-  connect.row_factory = sqlite3.Row
-  return connect
-
-
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-
-
 def get_db():
-  if not hasattr(g, 'attribute'):
-    g.attribute = connect_db()
-  return g.sqlite_db
-
-@app.before_request
-def before_request():
-  g.db = connect_db()
+    db = getattr(g,'db',None)
+    if db is None:
+      db =sqlite3.connect(db_location)
+      g.db = db
+      db.row_factory = sqlite3.Row
+      return db
 
 @app.teardown_request
 def teardown_request(exception):
@@ -42,45 +25,24 @@ def teardown_request(exception):
   if db is not None:
     db.close
 
-
-
-##################
-
-
-def reader():
-    page = []
+def init_db():
+  with app.app_context():
     db = get_db()
-    sql = "SELECT id, * FROM user"
-    for row in db.cursor().execute(sql):
-        page.append(str(row))
-    print page
+    with app.open_ressource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit
 
-@app.route("/test")
-def finaly():
-    print "finally"
-    db = get_db()
-    db.cursor().execute('insert into user VALUES ("2", "adam", "good at sql")')
-    db.commit()
-
-    page=[]
-    page.append('<html><ul>')
-    sql = "SELECT rowid, * FROM user ORDER BY password"
-    for row in db.cursor().execute(sql):
-         page.append('<li>')
-         page.append(str(row))
-         page.append('<li>')
-    page.append('</ul></html>')
-    return ''.join(page)
-
-
-#######################
+#def query_db(query, arg=(), one=False):
+#    db = get_db()
+#    cur = db.execute(query,args)
+#    rv = [dict((cur.description[idx][0], value) for idx, value in enumerate(row)) for row in cur.fetchall()]
+#    return (rv[0] if rv else None) if one else rv
 
 @app.route('/display')
 def print_users():
-    cursor = g.db.cursor()
-    cursor = g.db.execute('SELECT username,password FROM user ORDER BY id ASC')
-    entries = [dict(username=row[0],password=row[1]) for row in cursor.fetchall()]
-    print "working"
+    db = get_db()
+    cur = db.execute('SELECT username,password FROM user ORDER BY id ASC')
+    entries = [dict(username=row[0],password=row[1]) for row in cur.fetchall()]
     return render_template('displayUsers.html',entries=entries)
 
 
@@ -90,12 +52,20 @@ def add():
     if request.method == 'GET':
         return render_template ('createAccount.html')
     if request.method == 'POST':
-      db = get_db()
-      #insert in db
-      db.execute('INSERT INTO user (username,password) values (?,?)',[request.form['username'],request.form['password']])
-      db.commit()
-      flash('new entry added')
-      return render_template ('login.html')
+      error = []
+      form = request.form
+      #query = 'SELECT * FROM user WHERE username = ?'
+      #testUsername = query_db,(query, [request.form['username']])
+      #if testUsername:
+      #    error.append('please enter a diffrent username')
+      if request.form['password'] != request.form["confirm_password"]:
+          error.append("please enter same password")
+      if not error:
+          db = get_db()
+          #insert in db
+          db.cursor().execute('INSERT INTO user (username,password) values (?,?)',[request.form['username'],request.form['password']])
+          db.commit()
+          return render_template ('displayUsers.html')
 
 #THIS IS NOT A GOOD VERSION / TEST 
 @app.route("/discplay", methods={"GET","POST"})
@@ -150,3 +120,5 @@ def feed():
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
+
+
